0f0c5081e414bcd64eb3fa50a9d550ef9e923dce
Thomas de Visser <thomasdevisser@pm.me>
2023-01-06 00:04:12 +0100

    Added a clock and max framerate

--- game.py
+++ game.py
@@ -3,11 +3,20 @@ from sys import exit
 
 pygame.init()
 
+# Create the display and time
+pygame.display.set_caption('PyRunner')
 screen = pygame.display.set_mode((800, 400))
+clock = pygame.time.Clock()
 
 while True:
+    # Loop through all the events (player input)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    # Rerender the display
     pygame.display.update()
+
+    # Set the framerate to a max of 60fps
+    clock.tick(60)
