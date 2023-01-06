58963644dbcf46d4e53c9a633857934d96d596ff
Thomas de Visser <thomasdevisser@pm.me>
2023-01-06 19:51:34 +0100

    Added the snail NPC

--- game.py
+++ game.py
@@ -12,6 +12,9 @@ font = pygame.font.Font(None, 50)
 night_sky = pygame.image.load('assets/night-sky.png')
 night_ground = pygame.image.load('assets/night-ground.png')
 
+snail = pygame.image.load('assets/snail-in-moonlight.png')
+snail_position_x = 600
+
 title_surface = font.render('PyRunner', False, '#babec2')
 
 while True:
@@ -26,6 +29,9 @@ while True:
     screen.blit(night_ground, (0, 290))
 
     screen.blit(title_surface, (50, 50))
+
+    screen.blit(snail, (snail_position_x, 249))
+    snail_position_x -= 2
 
     # Rerender the display
     pygame.display.update()
