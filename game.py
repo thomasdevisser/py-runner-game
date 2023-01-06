f0a3cd402f4aa1e1dce33b39cfda9de975a9d7a7
Thomas de Visser <thomasdevisser@pm.me>
2023-01-06 19:04:20 +0100

    Created and loaded scene assets

--- game.py
+++ game.py
@@ -8,12 +8,18 @@ pygame.display.set_caption('PyRunner')
 screen = pygame.display.set_mode((800, 400))
 clock = pygame.time.Clock()
 
+night_sky = pygame.image.load('assets/night-sky.png')
+night_ground = pygame.image.load('assets/night-ground.png')
+
 while True:
     # Loop through all the events (player input)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    screen.blit(night_sky, (0, 0))
+    screen.blit(night_ground, (0, 290))
 
     # Rerender the display
     pygame.display.update()
