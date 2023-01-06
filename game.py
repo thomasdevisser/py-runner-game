e026fa2057aa651eb99fce40cdff30c97413d069
Thomas de Visser <thomasdevisser@pm.me>
2023-01-07 00:39:43 +0100

    Refactors and added collisions

--- game.py
+++ game.py
@@ -7,18 +7,24 @@ pygame.init()
 pygame.display.set_caption('PyRunner')
 screen = pygame.display.set_mode((800, 400))
 clock = pygame.time.Clock()
-font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
+# The title
+font = pygame.font.Font('font/Pixeltype.ttf', 50)
+title = font.render('Escaping Cindy', False, 'black')
+title_rect = title.get_rect(center=(400, 50))
+
+# The scene
 sky = pygame.image.load('graphics/sky.png').convert()
 ground = pygame.image.load('graphics/ground.png').convert()
 
-snail = pygame.image.load('graphics/snail/snail_1.png').convert_alpha()
-snail_rect = snail.get_rect(midbottom=(600, 300))
-
+# The player
 player = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player.get_rect(midbottom=(100, 300))
 
-title_surface = font.render('PyRunner', False, 'black')
+# The snail
+snail = pygame.image.load('graphics/snail/snail_1.png').convert_alpha()
+snail_rect = snail.get_rect(midbottom=(600, 300))
+
 
 while True:
     # Loop through all the events (player input)
@@ -27,18 +33,28 @@ while True:
             pygame.quit()
             exit()
 
-    # Add the background surfaces
+    # Render all the visuals to the screen
     screen.blit(sky, (0, 0))
     screen.blit(ground, (0, 300))
-
-    screen.blit(title_surface, (50, 50))
-
+    screen.blit(title, title_rect)
     screen.blit(snail, snail_rect)
     screen.blit(player, player_rect)
 
+    # Move the characters
     snail_rect.left -= 2
+
+    # Handle graphics going off-screen
     if snail_rect.left < -100:
         snail_rect.left = 900
+
+    # Check for rectangles colliding
+    if player_rect.colliderect(snail_rect):
+        print("BOOM, snail hits the player")
+
+    # Check for mouse colliding
+    mouse = pygame.mouse.get_pos()
+    if player_rect.collidepoint(mouse):
+        print("mouse on player")
 
     # Rerender the display
     pygame.display.update()
