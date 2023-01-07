c7924beb290866f559343529cc326c4fcf45a8da
Thomas de Visser <thomasdevisser@pm.me>
2023-01-07 15:04:25 +0100

    Added movement probably the wrong way

--- game.py
+++ game.py
@@ -20,11 +20,12 @@ ground = pygame.image.load('graphics/ground.png').convert()
 # The player
 player = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player.get_rect(midbottom=(100, 300))
+player_walking_left = False
+player_walking_right = False
 
 # The snail
 snail = pygame.image.load('graphics/snail/snail_1.png').convert_alpha()
 snail_rect = snail.get_rect(midbottom=(600, 300))
-
 
 while True:
     # Loop through all the events (player input)
@@ -32,6 +33,25 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                print("JUMP")
+
+            if event.key == pygame.K_d:
+                print("WALK RIGHT")
+                player_walking_right = True
+
+            if event.key == pygame.K_a:
+                print("WALK LEFT")
+                player_walking_left = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_d:
+                player_walking_right = False
+
+            if event.key == pygame.K_a:
+                player_walking_left = False
 
     # Render all the visuals to the screen
     screen.blit(sky, (0, 0))
@@ -42,6 +62,12 @@ while True:
 
     # Move the characters
     snail_rect.left -= 2
+
+    if player_walking_right:
+        player_rect.right += 2
+
+    if player_walking_left:
+        player_rect.left -= 2
 
     # Handle graphics going off-screen
     if snail_rect.left < -100:
@@ -55,6 +81,11 @@ while True:
     mouse = pygame.mouse.get_pos()
     if player_rect.collidepoint(mouse):
         print("mouse on player")
+
+    # Check for keys being pressed
+    # keys_pressed = pygame.key.get_pressed()
+    # if keys_pressed[pygame.K_SPACE]:
+    #     print('JUMP')
 
     # Rerender the display
     pygame.display.update()
